a59e2a456bc213c54ead099c34bda5e8991a8027
Didier2024-hue <didier.josephine.com>
2025-12-01 16:43:18 +0100

    MAJ dev 2025-12-01 16:43:16

--- ci/gitlab/scripts/lint.sh
+++ ci/gitlab/scripts/lint.sh
@@ -7,6 +7,6 @@ flake8 \
   api/ \
   api/scripts/app_api/ \
   --max-line-length=200 \
-  --ignore=E302,E305,E402,E501,W293,W391,F401,F841
+  --ignore=E302,E305,E402,E501,W293,W391,F401,F841,E704,E241,W291,E226,E303
 
 echo "✅ Lint API OK"
